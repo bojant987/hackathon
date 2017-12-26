9fc71774b91026cebb2a9206314ed515407ec546
Miloš Rašić <mrasic@gogoair.com>
2017-12-26 10:43:31 +0100

    initial action types, action creators and menu reducer stubs

--- src/Redux/constants/actionTypes.js
+++ src/Redux/constants/actionTypes.js
@@ -1,5 +1,20 @@
 import keyMirror from 'keymirror';
 
-export const actionTypes = keyMirror({
+export default keyMirror({
+	REQUEST_MENU: null,
+	RECEIVE_MENU: null,
+	MENU_ERROR: null,
 
+	SET_NAME: null,
+	SET_CURRENT_DAY: null,
+	ADD_ITEM: null,
+	REMOVE_ITEM: null,
+
+	SAVE_ORDER: null,
+	SAVE_ORDER_SUCCESS: null,
+	SAVE_ORDER_ERROR: null,
+
+	REQUEST_SUMMARY: null,
+	RECEIVE_SUMMARY: null,
+	SUMMARY_ERROR: null,
 });
---
+++ src/Redux/reducers/menu.js
@@ -0,0 +1,12 @@
+import actionTypes from '../constants/actionTypes';
+
+const initialState = {
+	data: null,
+	loading: false,
+};
+
+const actionHandlers = {
+	[actionTypes.REQUEST_MENU]: (state = initialState, action) => {
+		
+	},
+};
