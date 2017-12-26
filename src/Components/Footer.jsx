305c96256bd9246d1b04bfcf4963079731e75404
Jovan Milovanovic <jmilovanovic@gogoair.com>
2017-12-26 14:00:41 +0100

    item checked state >>> ant-card-overwrite--checked

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Layout } from 'antd';
 
 const AppFooter = props => {
-	return <Layout className="Footer">Order it app 2017</Layout>;
+	return <Layout className="Footer Navigation__brandText Navigation__brandText--smaller">Order it app 2017</Layout>;
 };
 
 export default AppFooter;
