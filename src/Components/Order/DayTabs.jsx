f622a00ba30b29247925c58780a40e7baf63df62
Bojan Todorovic <c.btodorovic@gogoair.com>
2017-12-26 11:19:30 +0100

    fixed tabs

--- src/Components/Order/DayTabs.jsx
+++ src/Components/Order/DayTabs.jsx
@@ -12,65 +12,75 @@ class DayTabs extends React.Component {
         super(props);
 
         this.state = {
-            activeKey: 'mon',
+            activeKey: '0',
         };
     }
 
+    static defaultProps = {
+        menu: {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+        }
+    };
+
     onChange = (activeKey) => {
         this.setState({activeKey});
-        this.props.switchDay(activeKey);
+        // this.props.switchDay(activeKey);
     };
 
     render() {
         return (
             <Tabs className="DayTabs" onChange={this.onChange} activeKey={this.state.activeKey}>
-                <TabPane tab="Mon" key="mon" className="DayTabsPane">
+                <TabPane tab="Mon" key="0" className="DayTabsPane">
                     <Row>
-                        {this.props.menu['mon'].map((item) => {
+                        {this.props.menu['0'].map((item) => {
                             return (
-                                <Col span={6}>
+                                <Col span={8}>
                                     <Card item={item} />
                                 </Col>
                             );
                         })}
                     </Row>
                 </TabPane>
-                <TabPane tab="Tue" key="tue">
+                <TabPane tab="Tue" key="1">
                     <Row>
-                        {this.props.menu['tue'].map((item) => {
+                        {this.props.menu['1'].map((item) => {
                             return (
-                                <Col span={6}>
+                                <Col span={8}>
                                     <Card item={item} />
                                 </Col>
                             );
                         })}
                     </Row>
                 </TabPane>
-                <TabPane tab="Wed" key="wed">
+                <TabPane tab="Wed" key="2">
                     <Row>
-                        {this.props.menu['wed'].map((item) => {
+                        {this.props.menu['2'].map((item) => {
                             return (
-                                <Col span={6}>
+                                <Col span={8}>
                                     <Card item={item} />
                                 </Col>
                             );
                         })}
                     </Row>
                 </TabPane>
-                <TabPane tab="Thu" key="thu">
+                <TabPane tab="Thu" key="3">
                     <Row>
-                        {this.props.menu['thu'].map((item) => {
+                        {this.props.menu['3'].map((item) => {
                             return (
-                                <Col span={6}>
+                                <Col span={8}>
                                     <Card item={item} />
                                 </Col>
                             );
                         })}
                     </Row>
                 </TabPane>
-                <TabPane tab="Fri" key="fri">
+                <TabPane tab="Fri" key="4">
                     <Row>
-                        {this.props.menu['fri'].map((item) => {
+                        {this.props.menu['4'].map((item) => {
                             return (
                                 <Col span={6}>
                                     <Card item={item} />
